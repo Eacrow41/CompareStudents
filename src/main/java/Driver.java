c77c25e89c25857dd3c21e9f00d1ea032302e1df
EstivnC <eacrow41@gmail.com>
2023-04-27 10:10:34 -0500

    first commit

---
+++ src/main/java/Driver.java
@@ -0,0 +1,22 @@
+public class Driver {
+    public static void main(String[] args) {
+        Student helen = new Student("Helen");
+        helen.addMarks("English", 63);
+        helen.addMarks("Math", 98);
+        helen.addMarks("History", 79);
+        Student leslie = new Student("Leslie");
+        leslie.addMarks("English", 89);
+        leslie.addMarks("Math", 83);
+        leslie.addMarks("History", 91);
+
+        if(helen.compareTo(leslie) > 0){
+            System.out.println(helen.getName() + " gets a higher total mark ");
+        }
+        else if(helen.compareTo(leslie) < 0) {
+            System.out.println(leslie.getName() + " gets a higher total mark ");
+        }
+        else {
+            System.out.println("Both students get the same total mark");
+        }
+    }
+}
